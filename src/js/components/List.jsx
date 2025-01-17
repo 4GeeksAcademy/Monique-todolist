34537c2d3e2385f98813eb5a8533dc094a363517
Monique Menezes <122538719+Niquemenezes@users.noreply.github.com>
2025-01-17 19:38:28 +0000

    todolist

---
+++ src/js/components/List.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+const List = () => {
+    const [entrada,setEntrada] = useState("");
+    const [cantidad, setCantidad] = useState([]);
+
+    function agregarItem (e) {
+      if (e.key === "Enter"){
+        if (entrada.trim() !== "") {
+          setCantidad(cantidad.concat(entrada));
+          setEntrada("");
+        }}
+  };
+
+  function eliminarItem(index) {
+    const nuevaLista = cantidad.filter((_,i) =>i !==index);
+    setCantidad(nuevaLista);
+  };
+
+  return(
+        <div className="container" >
+           <h1>Lista de tareas</h1>
+            <div className="listas">
+               <ul>
+                  <li>
+                     <input type="text" value={entrada} onChange={(e) =>setEntrada(e.target.value)} onKeyDown={agregarItem} placeholder="What needs to be done?"></input>
+                   </li>
+                  {cantidad.map((item, index) => (
+                    <li key={index} className="task-item">
+                      {item}{" "}
+                      <FontAwesomeIcon icon={faXmark} onClick={() => eliminarItem(index)} className="delete-icon" />
+                  </li> 
+                  ))}
+                </ul>
+                <div className="footer">{cantidad.length} item left </div>
+             </div>
+                         
+        </div>
+    );
+};
+
+export default List
